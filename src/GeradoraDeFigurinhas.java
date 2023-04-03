c8026f9a3c5a6bebe4e5bd393011d8f91b23dbe1
felipejrfonseca <felipe.jr.fonseca@gmail.com>
2023-04-03 10:59:00 -0300

    Aula 02 - Gerando figurinhas para WhatsApp
    Aula 2 +Desafios

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,6 +1,11 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.awt.FontMetrics;
 import java.io.File;
 import java.io.IOException;
 import java.awt.Font;
@@ -28,11 +33,33 @@ public class GeradoraDeFigurinhas {
         //Configurar a fonte
         Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 70);
         graphics.setFont(fonte);
-        graphics.setColor(Color.RED);
+        graphics.setColor(Color.DARK_GRAY);
 
         //escrever uma frase na nova imagem
-        graphics.drawString("TOPZERA", 100, novaAltura-100);
 
+        String nomeDoArquivo2 = nomeDoArquivo.replace(".png", "");
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D tamanhoTexto = fontMetrics.getStringBounds(nomeDoArquivo2, graphics);
+        int larguraTexto = (int) tamanhoTexto.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        /*int alturaTexto = (int) tamanhoTexto.getHeight();
+        int posicaoTextoY = (altura - alturaTexto) / 2;*/
+        graphics.drawString( nomeDoArquivo2, posicaoTextoX, novaAltura-100);
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        TextLayout textLayout = new TextLayout(nomeDoArquivo2, fonte, fontRenderContext);
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, novaAltura-100);
+        graphics.setTransform(transform);
+
+        BasicStroke outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.lightGray);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+        
         //escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File("ImersaoJavaAluraAPI/src/saida", nomeDoArquivo));
 
